73edf3f8344052fad74e9ad45a17cb353e82f241
AiDSl <aaiddssl@gmail.com>
2025-04-12 18:40:44 +0800

    hehe,just switch back to slf4j
    idk but just doing it anyway.

--- src/main/java/xyz/bedly_nerfed_sleeping/bedly_nerfed_sleeping.java
+++ src/main/java/xyz/bedly_nerfed_sleeping/bedly_nerfed_sleeping.java
@@ -2,19 +2,29 @@ package xyz.bedly_nerfed_sleeping;
 
 import net.minecraftforge.fml.common.Mod;
 
+/*
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+*/
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(bedly_nerfed_sleeping.MODID)
 public class bedly_nerfed_sleeping
 {
     public static final String MODID = "bedly_nerfed_sleeping";
+    /*
+    //log4j logger
     private static final Logger LOGGER = LogManager.getLogger(MODID);
+    */
+   //slf4j logger
+
+   private static final Logger LOGGER = LogUtils.getLogger();
 
     public bedly_nerfed_sleeping()
     {
-        LOGGER.log(Level.INFO,"Reduced!");
+        LOGGER.info("slf4j!");
     }
 }
