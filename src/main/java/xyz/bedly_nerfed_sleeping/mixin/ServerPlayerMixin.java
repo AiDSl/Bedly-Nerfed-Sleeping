f56e474a2ea4ec3a8527e716f1a96089c852ca07
AiDSl <aaiddssl@gmail.com>
2025-04-12 22:25:22 +0800

    wtf
    It pass the compile but did not exist in jar file :/

--- src/main/java/xyz/bedly_nerfed_sleeping/mixin/ServerPlayerMixin.java
+++ src/main/java/xyz/bedly_nerfed_sleeping/mixin/ServerPlayerMixin.java
@@ -1,9 +1,17 @@
-/*package com.example.examplemod.mixin;
+package com.example.examplemod.mixin;
+
+
+
 
 import xyz.bedly_nerfed_sleeping.bedly_nerfed_sleeping;
+import net.minecraft.world.phys.Vec3;
 
-
-import net.minecraft.world.phys.AABB;
+//mixin org
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 //mixins
 import net.minecraft.server.level.ServerPlayer;
 	//startSleepInBed
@@ -11,35 +19,37 @@ import net.minecraft.server.level.ServerPlayer;
 	import com.mojang.datafixers.util.Either;
 
 	//getEntitiesOfClass
+	import net.minecraft.world.level.EntityGetter;
 	import java.lang.Class;
-	//import net.minecraft.world.phys.AABB;
+	import net.minecraft.world.phys.AABB;
 	import java.util.function.Predicate;
 	import java.util.List;
+		
 
 
 
 
-import org.slf4j.Logger;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin {
 	
-    @ModifyArg( method="startSleepInBed(Lnet/minecraft/core/BlockPos)Lcom/mojang/datafixers/util/Either;", at = @At(value = "INVOKE", target = "getEntitiesOfClass(Ljava/lang/Class,Lnet/minecraft/world/phys/AABB,Ljava/util/function/Predicate)Ljava/util/List"), index = 1)
+    @ModifyArg(
+		method="startSleepInBed(Lnet/minecraft/core/BlockPos;)Lcom/mojang/datafixers/util/Either;",
+		at=@At(value="INVOKE",target="Lnet/minecraft/world/level/EntityGetter;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"),
+		index = 1
+	)
     private AABB bigger_monster_range(AABB old_range)
 	{
 		Vec3 bottom_center=old_range.getCenter();
+		double r=0;//range
+
+		bedly_nerfed_sleeping.LOGGER.info("("+bottom_center.x+","+bottom_center.y+","+bottom_center.z+")");
 
 		return new AABB(
-			bottom_center.x()-64, bottom_center.y()-64, bottom_center.z()-64,
-			bottom_center.x()+64, bottom_center.y()+64, bottom_center.z()+64
+			bottom_center.x()-r, bottom_center.y()-r, bottom_center.z()-r,
+			bottom_center.x()+r, bottom_center.y()+r, bottom_center.z()+r
 		);
     }
 
-}*/
+}
