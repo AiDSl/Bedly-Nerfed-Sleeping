a3e75fd0ef0f4b483eb169ee501d705cf009f5de
AiDSl <aaiddssl@gmail.com>
2025-04-13 18:39:03 +0800

    HELL YAAA
    finally working :>

--- src/main/java/xyz/bedly_nerfed_sleeping/mixin/ServerPlayerMixin.java
+++ src/main/java/xyz/bedly_nerfed_sleeping/mixin/ServerPlayerMixin.java
@@ -2,50 +2,68 @@ package xyz.bedly_nerfed_sleeping.mixin;
 
 import xyz.bedly_nerfed_sleeping.bedly_nerfed_sleeping;
 import net.minecraft.world.phys.Vec3;
-
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.Entity;
+//import java.util.List;
 //mixin org
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 //mixins
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 	//startSleepInBed
 	import net.minecraft.core.BlockPos;
 	import com.mojang.datafixers.util.Either;
 
 	//getEntitiesOfClass
-	import net.minecraft.world.level.EntityGetter;
+	import net.minecraft.world.level.Level;
 	import java.lang.Class;
 	import net.minecraft.world.phys.AABB;
 	import java.util.function.Predicate;
 	import java.util.List;
 		
-
-
+	//return
+	import net.minecraft.world.entity.player.Player.BedSleepingProblem;
+	import net.minecraft.util.Unit;
 
 
 
 
 @Mixin(ServerPlayer.class)
-public abstract class ServerPlayerMixin {
+public class ServerPlayerMixin{
 	
-    @ModifyArg(
+	@ModifyArg(
 		method="startSleepInBed(Lnet/minecraft/core/BlockPos;)Lcom/mojang/datafixers/util/Either;",
-		at=@At(value="INVOKE",target="Lnet/minecraft/world/level/EntityGetter;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;"),
-		index = 1
+		at=@At(value="INVOKE",target="Lnet/minecraft/world/level/Level;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;")
 	)
     private AABB bigger_monster_range(AABB old_range)
 	{
 		Vec3 bottom_center=old_range.getCenter();
-		double r=0;
-		bedly_nerfed_sleeping.LOGGER.info("("+bottom_center.x+","+bottom_center.y+","+bottom_center.z+")");
-
+		double r=32;
 		return new AABB(
 			bottom_center.x()-r, bottom_center.y()-r, bottom_center.z()-r,
 			bottom_center.x()+r, bottom_center.y()+r, bottom_center.z()+r
 		);
-    }
+		
+    };
+
+	@ModifyVariable(
+		method="startSleepInBed(Lnet/minecraft/core/BlockPos;)Lcom/mojang/datafixers/util/Either;",
+		at=@At(value="STORE",target="Lnet/minecraft/world/level/Level;getEntitiesOfClass(Ljava/lang/Class;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;")
+	)
+    private List<Monster> glow_the_clowns(List<Monster> list)
+	{
+		for(Monster m:list)
+		{m.addEffect(new MobEffectInstance(MobEffects.GLOWING,20*3));};
+		
+		return list;
+    };
+
 
 }
